80d014f65f6a247438d5ba4953c8936129ab0f4b
gabicilfone <cilfonegabriel@gmail.com>
2023-04-24 18:06:20 -0300

    add method set_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,9 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create; end
+
+  # Use callbacks to share common setup or constraints between actions.
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
