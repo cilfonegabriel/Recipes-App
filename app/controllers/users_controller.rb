6507bd6e52a7632b4646dfce5a9fdbacc367d011
gabicilfone <cilfonegabriel@gmail.com>
2023-04-24 16:27:54 -0300

    fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,16 @@
 class UsersController < ApplicationController
+  # GET /users or /users.json
+  def index; end
 
-    # GET /users or /users.json
-    def index
-    end
+  # GET /users/1 OR /users/1.json
+  def show; end
 
-    # GET /users/1 OR /users/1.json
-    def show
-    end
+  # GET /users/new
+  def new; end
 
-    # GET /users/new
-    def new
-    end
+  # GET /users/1/edit
+  def edit; end
 
-    # GET /users/1/edit
-    def edit
-    end
-
-    # POST /users or /users.json
-    def create
-    end
+  # POST /users or /users.json
+  def create; end
 end
