675aba9174b65fa55e0825b13f520a837d8f9177
gabicilfone <cilfonegabriel@gmail.com>
2023-04-25 14:31:43 -0300

    add controller and route for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :foods, only: [:show, :edit, :update, :destroy] 
 
   root "users#show"
 end
