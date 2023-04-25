ecbab8d6e23726ce997a70d874a3b80be498d238
gabicilfone <cilfonegabriel@gmail.com>
2023-04-25 16:40:58 -0300

    changes in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-    resources :foods, only: [:show, :edit, :update, :destroy] 
+    resources :foods, only: [:index, :show, :edit, :update, :destroy] 
   end
 
   root "users#show"
