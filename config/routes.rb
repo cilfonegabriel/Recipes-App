4e1257f04e63d4a6b968fe0fd5faaa349ab206b5
gabicilfone <cilfonegabriel@gmail.com>
2023-04-26 18:49:27 -0300

    routing correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] 
-  resources :foods, only: [:index, :show, :edit, :update, :destroy] 
   resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  
+
+  resources :foods, only: [:index, :new, :create, :show, :update, :destroy] 
 
   root "users#show"
 end
