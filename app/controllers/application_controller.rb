c4c5d6c0bb7821431c9ddf5c095aa753bf8bcf9d
gabicilfone <cilfonegabriel@gmail.com>
2023-04-26 01:21:12 -0300

    add params for sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:email, :encrypted_password)
+      user_params.permit(:email, :password, :remember_me)
+8
     end
 
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
