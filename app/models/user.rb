b63fc50a18c3f92d9dbd8e9deee45b1c6123f01d
gabicilfone <cilfonegabriel@gmail.com>
2023-04-26 18:46:49 -0300

    locate line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_many :foods
 
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :recipes
+  has_many :foods
 
   validates :name, presence: true
   validates :email, presence: true
