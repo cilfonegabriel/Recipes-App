1ecafa7e952780db337723a1bf736c7593cfbdd7
gabicilfone <cilfonegabriel@gmail.com>
2023-04-26 20:23:14 -0300

    delete unnecesary line

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 150 }
   validates :measurement_unit, presence: true, length: { maximum: 10 }
