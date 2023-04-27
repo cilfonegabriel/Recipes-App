79a9f7c4a0c8c62f8a736e434a7850a00405b4eb
gabicilfone <cilfonegabriel@gmail.com>
2023-04-27 00:38:56 -0300

    add shoping-list and recipe_foods of the route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] 
   resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  resources :recipe_foods [:index, :new, :create, :show, :update, :destroy] 
   
+  get 'shoping-list', to: 'foods#shoping_list'
 
   resources :foods, only: [:index, :new, :create, :show, :update, :destroy] 
 
