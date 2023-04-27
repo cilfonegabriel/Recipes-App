1249f7dd8406449e0a3c690c68b4defab01f1cfb
gabicilfone <cilfonegabriel@gmail.com>
2023-04-27 00:37:46 -0300

    add controller and model for recipe_foods

--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -6,40 +6,44 @@ class FoodsControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get user_foods_url(@user)
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_user_food_url(@user)
     assert_response :success
   end
 
-  test "should create food" do
+  test 'should create food' do
     assert_difference('Food.count') do
-      post user_foods_url(@user), params: { food: { name: @food.name, measurement_unit: @food.measurement_unit, price: @food.price, quantity: @food.quantity } }
+      post user_foods_url(@user),
+           params: { food: { name: @food.name, measurement_unit: @food.measurement_unit, price: @food.price,
+                             quantity: @food.quantity } }
     end
 
     assert_redirected_to food_url(Food.last)
   end
 
-  test "should show food" do
+  test 'should show food' do
     get user_food_url(@user, @food)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_user_food_url(@user, @food)
     assert_response :success
   end
 
-  test "should update food" do
-    patch user_food_url(@user, @food), params: { food: { name: @food.name, measurement_unit: @food.measurement_unit, price: @food.price, quantity: @food.quantity } }
+  test 'should update food' do
+    patch user_food_url(@user, @food),
+          params: { food: { name: @food.name, measurement_unit: @food.measurement_unit, price: @food.price,
+                            quantity: @food.quantity } }
     assert_redirected_to food_url(@food)
   end
 
-  test "should destroy food" do
+  test 'should destroy food' do
     assert_difference('Food.count', -1) do
       delete user_food_url(@user, @food)
     end
