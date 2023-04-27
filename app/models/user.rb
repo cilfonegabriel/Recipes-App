f9fb3c34e458b5256fa652f66717d09a3389f62c
gabicilfone <cilfonegabriel@gmail.com>
2023-04-27 00:39:56 -0300

    fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
