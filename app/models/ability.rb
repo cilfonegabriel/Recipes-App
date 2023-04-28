0c37fc8afa1bf5c78dedc9030c9b2e3b33fee4b4
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-28 04:40:29 -0600

    Implement auth for publicToggle

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,13 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    can(:read, Recipe, public: true)
+    can(:read, User)
+
+    return unless user.present?
+
+    can(%i[read create update destroy], Recipe, user:)
+    can(%i[read create destroy], Food, user:)
+  end
 end
