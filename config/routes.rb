00decc8b473845b12cd5ed694d76d14a2719e44e
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-28 06:42:14 -0600

    Delete Users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show] 
   resources :recipes, only: [:index, :new, :create, :show, :update, :destroy]
   resources :recipe_foods, only: [:new, :create,:update, :destroy] 
   
