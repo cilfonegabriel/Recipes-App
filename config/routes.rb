11e89da72908ab63927c581cc3fc2a21da90deb2
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-28 09:12:33 -0600

    Create Ingredients routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy]
-  resources :recipe_foods, only: [:new, :create,:update, :destroy] 
+  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+    resources :ingredients, only: [:new, :create]
+  end
+  resources :foods, only: [:index, :new, :create, :show, :update, :destroy] 
   
   get 'shoping-list', to: 'foods#shoping_list'
   get 'public-recipes', to: 'recipes#public_recipes'
 
-  resources :foods, only: [:index, :new, :create, :show, :update, :destroy] 
 
   root "recipes#public_recipes"
 end
