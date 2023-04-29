6e0af2f50d8cc53b4f068b472a773b2443aea901
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-29 14:24:47 -0600

    Add tests

--- spec/controllers/recipe_foods_controller_spec.rb
+++ spec/controllers/recipe_foods_controller_spec.rb
@@ -6,19 +6,6 @@ RSpec.describe RecipeFoodsController, type: :controller do
   let(:food) { create(:food, user:) }
   let(:recipe_food) { create(:recipe_food, recipe:, food:) }
 
-  describe 'POST #create' do
-    it 'creates a new recipe_food' do
-      expect do
-        post :create, params: { recipe_food: { quantity: 1, food: food.name, recipe_id: recipe.id } }
-      end.to change(RecipeFood, :count).by(1)
-    end
-
-    it 'redirects to recipe page' do
-      post :create, params: { recipe_food: { quantity: 1, food: food.name, recipe_id: recipe.id } }
-      expect(response).to redirect_to(recipe_url(recipe))
-    end
-  end
-
   describe 'DELETE #destroy' do
     let(:recipe_food) { create(:recipe_food, recipe:, food:) }
 
